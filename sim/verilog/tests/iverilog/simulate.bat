3bcf04e9f806f7cc493ac67992225b54c681122e
PacoReinaCampo <frareicam@gmail.com>
2021-02-12 13:01:31 +0100

    Working in Peripheral-NoC

--- sim/verilog/tests/iverilog/simulate.bat
+++ sim/verilog/tests/iverilog/simulate.bat
@@ -1,5 +1,5 @@
 @echo off
-call ../../../../../../settings64_iverilog.bat
+call ../../../../settings64_iverilog.bat
 
 iverilog -g2012 -o system.vvp -c system.vc -s mpsoc_noc_testbench
 vvp system.vvp
