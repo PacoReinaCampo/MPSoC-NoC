5d51ad5f725fda0a111cfb441806cb5af6d48454
PacoReinaCampo <frareicam@gmail.com>
2021-02-15 23:00:26 +0100

    Working in Peripheral-NoC

--- sim/verilog/uvm/noc/3d/msim/simulate.bat
+++ sim/verilog/uvm/noc/3d/msim/simulate.bat
@@ -2,6 +2,6 @@
 call ../../../../../../settings64_msim.bat
 
 vlib work
-vlog -sv -stats=none +incdir+../../../../../../uvm/src -f system.vc
+vlog -sv -stats=none -f system.vc
 vsim -c -do run.do work.testbench
 pause
