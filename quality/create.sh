4568163321a58e5e643b33d31a216ccc32b5d850
PacoReinaCampo <frareicam@gmail.com>
2022-08-02 23:53:18 +0200

    Working in Peripheral-NoC

---
+++ quality/create.sh
@@ -0,0 +1,21 @@
+mkdir scope
+mkdir normative_references
+mkdir terms_and_definitions
+mkdir context_of_the_organization
+mkdir leadership
+mkdir planning
+mkdir support
+mkdir operation
+mkdir performance_evaluation
+mkdir improvement
+
+touch scope/scope.md
+touch normative_references/normative_references.md
+touch terms_and_definitions/terms_and_definitions.md
+touch context_of_the_organization/context_of_the_organization.md
+touch leadership/leadership.md
+touch planning/planning.md
+touch support/support.md
+touch operation/operation.md
+touch performance_evaluation/performance_evaluation.md
+touch improvement/improvement.md
