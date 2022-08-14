b9c38c8454981da39521f8be4b04f4e1e067f31b
PacoReinaCampo <frareicam@gmail.com>
2022-08-14 19:49:56 +0200

    Working in Peripheral-NoC 🔑

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 git config --global user.email "frareicam@gmail.com"
 git config --global user.name "PacoReinaCampo"
 git add *
-git commit -m "Working in Peripheral-NoC"
+git commit -m "Working in Peripheral-NoC 🔑"
 git push origin master
