4c0aeeb45b2a8765b3d285d9e3114b128b78b1b5
PacoReinaCampo <frareicam@gmail.com>
2023-02-05 00:02:30 +0100

    Working in Peripheral-NoC 🔑

--- bench/verilog/tests/uvm/package.sv
+++
@@ -1,8 +0,0 @@
-`include "peripheral_sequence_item.sv"
-`include "peripheral_sequence.sv"
-`include "peripheral_sequencer.sv"
-`include "peripheral_driver.sv"
-`include "peripheral_monitor.sv"
-`include "peripheral_scoreboard.sv"
-`include "peripheral_agent.sv"
-`include "peripheral_enviroment.sv"
