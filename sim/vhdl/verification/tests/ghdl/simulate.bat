07c5ee265731ee9326f351533edfaf29e4665f56
PacoReinaCampo <frareicam@gmail.com>
2023-04-12 16:23:32 +0200

    Working in Peripheral-NoC 🔑

--- sim/vhdl/verification/tests/ghdl/simulate.bat
+++ sim/vhdl/verification/tests/ghdl/simulate.bat
@@ -45,19 +45,24 @@
 @echo off
 call ../../../../settings64_ghdl.bat
 
-ghdl -a --std=08 ../../../../rtl/vhdl/code/pkg/core/vhdl_pkg.vhd
-ghdl -a --std=08 ../../../../rtl/vhdl/code/peripheral/main/peripheral_arbiter_rr.vhd
-ghdl -a --std=08 ../../../../rtl/vhdl/code/peripheral/main/peripheral_noc_buffer.vhd
-ghdl -a --std=08 ../../../../rtl/vhdl/code/peripheral/main/peripheral_noc_demux.vhd
-ghdl -a --std=08 ../../../../rtl/vhdl/code/peripheral/main/peripheral_noc_mux.vhd
-ghdl -a --std=08 ../../../../rtl/vhdl/code/peripheral/main/peripheral_noc_vchannel_mux.vhd
-ghdl -a --std=08 ../../../../rtl/vhdl/code/peripheral/router/peripheral_noc_router_input.vhd
-ghdl -a --std=08 ../../../../rtl/vhdl/code/peripheral/router/peripheral_noc_router_lookup_slice.vhd
-ghdl -a --std=08 ../../../../rtl/vhdl/code/peripheral/router/peripheral_noc_router_lookup.vhd
-ghdl -a --std=08 ../../../../rtl/vhdl/code/peripheral/router/peripheral_noc_router_output.vhd
-ghdl -a --std=08 ../../../../rtl/vhdl/code/peripheral/router/peripheral_noc_router.vhd
-ghdl -a --std=08 ../../../../rtl/vhdl/code/peripheral/topology/peripheral_noc_mesh4d.vhd
-ghdl -a --std=08 ../../../../bench/vhdl/code/tests/peripheral_noc_testbench.vhd
+ghdl -a --std=08 ../../../../../rtl/vhdl/code/pkg/core/vhdl_pkg.vhd
+
+ghdl -a --std=08 ../../../../../rtl/vhdl/code/peripheral/main/peripheral_arbiter_rr.vhd
+ghdl -a --std=08 ../../../../../rtl/vhdl/code/peripheral/main/peripheral_noc_buffer.vhd
+ghdl -a --std=08 ../../../../../rtl/vhdl/code/peripheral/main/peripheral_noc_demux.vhd
+ghdl -a --std=08 ../../../../../rtl/vhdl/code/peripheral/main/peripheral_noc_mux.vhd
+ghdl -a --std=08 ../../../../../rtl/vhdl/code/peripheral/main/peripheral_noc_vchannel_mux.vhd
+
+ghdl -a --std=08 ../../../../../rtl/vhdl/code/peripheral/router/peripheral_noc_router_input.vhd
+ghdl -a --std=08 ../../../../../rtl/vhdl/code/peripheral/router/peripheral_noc_router_lookup_slice.vhd
+ghdl -a --std=08 ../../../../../rtl/vhdl/code/peripheral/router/peripheral_noc_router_lookup.vhd
+ghdl -a --std=08 ../../../../../rtl/vhdl/code/peripheral/router/peripheral_noc_router_output.vhd
+ghdl -a --std=08 ../../../../../rtl/vhdl/code/peripheral/router/peripheral_noc_router.vhd
+
+ghdl -a --std=08 ../../../../../rtl/vhdl/code/peripheral/topology/peripheral_noc_mesh4d.vhd
+
+ghdl -a --std=08 ../../../../../bench/vhdl/code/tests/peripheral_noc_testbench.vhd
+
 ghdl -m --std=08 peripheral_noc_testbench
 ghdl -r --std=08 peripheral_noc_testbench --ieee-asserts=disable-at-0 --disp-tree=inst > peripheral_noc_testbench.tree
 pause
