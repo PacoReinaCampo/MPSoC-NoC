ad5f03b4cb65a59fe4c72ab8044ee0d47706e9a6
PacoReinaCampo <frareicam@gmail.com>
2023-10-05 11:13:16 +0400

    Working in Peripheral-NoC 🔑

--- sim/verilog/validation/osvvm/bfm/ahb3/ghdl/simulate.bat
+++ sim/verilog/validation/osvvm/bfm/ahb3/ghdl/simulate.bat
@@ -44,7 +44,7 @@
 
 @echo off
 call ../../../../../../../settings64_ghdl.bat
-sh system.s
+sh system.g
 
 ghdl -e --std=08 peripheral_osvvm_testbench
 ghdl -r --std=08 peripheral_osvvm_testbench --ieee-asserts=disable-at-0 --vcd=peripheral_osvvm_testbench.vcd --wave=system.ghw --stop-time=1ms
